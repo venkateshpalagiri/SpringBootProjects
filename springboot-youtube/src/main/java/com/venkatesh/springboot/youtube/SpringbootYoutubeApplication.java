30ca9cb49ffea4abce5a66cd7dc8d09933feb119
venkatesh <venkatesh@gmail.com>
2022-05-07 10:45:11 -0400

    Spring initializer

---
+++ springboot-youtube/src/main/java/com/venkatesh/springboot/youtube/SpringbootYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.venkatesh.springboot.youtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootYoutubeApplication.class, args);
+	}
+
+}
